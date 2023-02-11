c244394e4362d062807b3cd124edc28836a50297
Sinchana13 <107702267+Sinchana13@users.noreply.github.com>
2023-02-11 18:49:31 +0530

    Add files via upload

---
+++ utils/fertilizer.py
@@ -0,0 +1,90 @@
+fertilizer_dic = {
+        'NHigh': """The N value of soil is high and might give rise to weeds.
+        <br/> Please consider the following suggestions:
+
+        <br/><br/> 1. <i> Manure </i> 
+
+        <br/> 2. <i>Coffee grinds </i>
+
+        <br/>3. <i>Plant nitrogen fixing plants</i>
+
+        <br/>4. Plant ‘green manure’ crops like cabbage, corn and brocolli
+
+        <br/>5. <i>Use mulch (wet grass) while growing crops</i> """,
+
+        'Nlow': """The N value of your soil is low.
+        <br/> Please consider the following suggestions:
+        <br/><br/> 1. <i>Add sawdust or fine woodchips to your soil</i> 
+        <br/>2. <i>Plant heavy nitrogen feeding plants</i> 
+
+        <br/>3. <i>Water</i> 
+
+        <br/>4. <i>Sugar</i> 
+
+        <br/>5. Add composted manure to the soil.
+
+        <br/>6. Plant Nitrogen fixing plants like peas or beans.
+
+        <br/>7. <i>Use NPK fertilizers with high N value.
+
+        <br/>8. <i>Do nothing</i> """,
+
+        'PHigh': """The P value of your soil is high.
+        <br/> Please consider the following suggestions:
+
+        <br/><br/>1. <i>Avoid adding manure</i> 
+
+        <br/>2. <i>Use only phosphorus-free fertilizer</i> 
+        <br/>3. <i>Water your soil</i> 
+
+        <br/>4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
+
+        <br/>5. Use crop rotations to decrease high phosphorous levels""",
+
+        'Plow': """The P value of your soil is low.
+        <br/> Please consider the following suggestions:
+
+        <br/><br/>1. <i>Bone meal</i> .
+
+        <br/>2. <i>Rock phosphate</i> 
+
+        <br/>3. <i>Phosphorus Fertilizers</i> 
+
+        <br/>4. <i>Organic compost</i> 
+
+        <br/>5. <i>Manure</i>
+
+        <br/>6. <i>Clay soil</i>
+
+        <br/>7. <i>Ensure proper soil pH</i>
+
+        <br/>8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. 
+
+        <br/>9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. """,
+
+        'KHigh': """The K value of your soil is high</b>.
+        <br/> Please consider the following suggestions:
+
+        <br/><br/>1. <i>Loosen the soil</i>
+
+        <br/>2. <i>Sift through the soil</i>
+
+        <br/>3. Stop applying potassium-rich commercial fertilizer. 
+
+        <br/>4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.
+
+        <br/>5. Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.
+
+        <br/>6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.
+        """,
+
+        'Klow': """The K value of your soil is low.
+        <br/>Please consider the following suggestions:
+
+        <br/><br/>1. Mix in muricate of potash or sulphate of potash
+        <br/>2. Try kelp meal or seaweed
+        <br/>3. Try Sul-Po-Mag
+        <br/>4. Bury banana peels an inch below the soils surface
+        <br/>5. Use Potash fertilizers since they contain high values potassium
+        """
+    }
